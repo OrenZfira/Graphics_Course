6e8cdef90ccd23d1b0292322d461a7d5b5d6206f
Sheer Maoz <sheermaoz@gmail.com>
2022-07-28 13:21:08 +0300

    added fofColur change

--- tutorial/shaders/basicShaderTex.glsl
+++ tutorial/shaders/basicShaderTex.glsl
@@ -9,6 +9,7 @@ uniform vec4 lightColor;
 uniform sampler2D sampler1;
 uniform vec4 lightDirection;
 uniform int fog;
+uniform vec4 fogColour;
 uniform vec4 camera;
 
 
@@ -16,7 +17,6 @@ void main()
 {
 	float fog_max = 30.0;
 	float fog_min = 5.0;
-	vec4  fog_colour = vec4(0.8, 0.8, 0.8, 0);
 	float dist = length(position0.xyz-camera.xyz);
 
 	// Calculate fog
@@ -25,6 +25,6 @@ void main()
 	fog_factor = fog_factor*fog_factor;
 	fog_factor = clamp(fog_factor, 0.2, 1);
 
-	gl_FragColor = mix(vec4(texture2D(sampler1, texCoord0).xyz,0), fog_colour, (1-fog_factor)*fog); //you must have gl_FragColor
+	gl_FragColor = mix(vec4(texture2D(sampler1, texCoord0).xyz,0), fogColour, (1-fog_factor)*fog); //you must have gl_FragColor
 }
 
--- tutorial/shaders/cubemapShader.glsl
+++ tutorial/shaders/cubemapShader.glsl
@@ -10,13 +10,14 @@ uniform vec4 lightColor;
 uniform samplerCube sampler1;
 uniform vec4 lightDirection;
 uniform int fog;
+uniform vec4 fogColour;
 
 out vec4 Color;
 void main()
 {
 	vec4  fog_colour = vec4(0.8, 0.8, 0.8, 0);
 
-	Color = mix(vec4(texture(sampler1, lookat).xyz,0), fog_colour, 0.8*fog);
+	Color = mix(vec4(texture(sampler1, lookat).xyz,0), fogColour, 0.8*fog);
 }  
 
 
